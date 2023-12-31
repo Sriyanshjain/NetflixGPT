5be445b0fd6a73de413e67693936bffc485ab05f
sriyanshjain <shreyanshjain036@gmail.com>
2023-12-31 09:51:28 +0530

    responsive on ipad pro

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,7 +7,7 @@ const SecondaryContainer = () => {
     const movies=useSelector((store)=>store.movies);
   return (
    movies && <div className='bg-neutral-900 pb-8'>
-        <div className='sm:mt-0  lg:-mt-30 pl-12 relative z-20  '>
+        <div className='sm:mt-0 md:-mt-20 lg:-mt-36 xl:-mt-52 pl-12 relative z-20  '>
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
         <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies}/>
         <MovieList title={"Popular"} movies={movies.popularMovies}/>
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,9 +3,9 @@ import { InformationCircleIcon } from '@heroicons/react/24/outline'
 import { PlayIcon } from '@heroicons/react/24/solid'
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='w-full aspect-video sm:pt-[20%] pt-[15%]  px-6 md:px-24 absolute text-white bg-gradient-to-r from-black'>
-        <h1 className='text-2xl sm:text-3xl lg:text-6xl font-bold'>{title}</h1>
-        <p className='hidden md:inline-block py-6 sm:text-sm lg:text-lg md:w-3/4 lg:w-1/2'>{overview}</p>
+    <div className='w-full aspect-video sm:pt-[20%] md:pt-[12%] pt-[15%] lg:pt-[13%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black'>
+        <h1 className='text-2xl sm:text-3xl md:text-2xl lg:text-5xl font-bold'>{title}</h1>
+        <p className='hidden md:inline-block py-6 sm:text-sm lg:text-lg  md:w-3/4 lg:w-1/2'>{overview}</p>
         <div className='flex mt-4'>
         <button className='bg-zinc-500 text-black bg-white px-4 py-1  md:px-7 md:py-2 bg-opacity-80 hover:bg-opacity-100 rounded-md '>
              <PlayIcon className='inline-block px-1 p  w-7 h-7 md:w-10 md:h-10'/>
