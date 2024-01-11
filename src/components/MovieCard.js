c053dba4fd67d6be506404b6e9e187d2e1ef4398
sriyanshjain <shreyanshjain036@gmail.com>
2024-01-11 09:33:03 +0530

    movie card animate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,7 @@ const MovieCard = ({posterPath,movieId}) => {
   if(!posterPath)
   return
   return (
-    <div className='w-36 md:w-48 pr-4 aspect-[2/3] cursor-pointer'>
+    <div className='w-36 md:w-48 pr-4 aspect-[2/3] cursor-pointer hover:scale-95 origin-center transition-all duration-100 ease-in'>
         <Link to={`/movie/${movieId}`}><img alt='Movie card' src={TMDB_CDN_URL+'w500/'+posterPath}></img></Link>
     </div>
   )
